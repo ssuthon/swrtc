b1c87ff3564d6b025de233c33186d8fd56f33bcf
suthon <suthon@gmail.com>
2015-03-19 14:36:51 +0700

    caller

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-var app = require('express')();
+var express = require('express')
+var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+
+app.use(express.static(__dirname + '/public'))
 
 server.listen(80);
 
 
 io.on('connection', function (socket) {
   socket.on('bc', function (data) {
+  	console.log('boardcasting from ' + data.by);
     socket.broadcast.emit(data);
   });
 });
